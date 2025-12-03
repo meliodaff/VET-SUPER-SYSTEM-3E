1ecf60ba325cb89b18483486dba71da1dd5c9f85
ARMEYAYA <141784034+ARMEYAYA@users.noreply.github.com>
2025-12-03 08:26:30 +0800

    final na talga to

--- APPOINTMENT/appointment/admin_page/services.php
+++ APPOINTMENT/appointment/admin_page/services.php
@@ -46,7 +46,6 @@ include '../includes/db.php';
       <img src="image/community.png" class="nav-icon" alt="Dashboard Icon">  
         Employee Portal
       </a>
-
       <!-- Logout at bottom using padding trick -->
       <a href="http://localhost:5173/login" class="nav-logout">
       <img src="image/arrow-out-right-square-half.png" class="nav-icon" alt="Dashboard Icon">  
